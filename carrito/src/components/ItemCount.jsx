2696f976ba7fc101296644860907ed343f181710
aleresk <alejandro.resk@outlook.com>
2022-06-21 19:00:38 -0300

    carrio version 1

--- carrito/src/components/ItemCount.jsx
+++ carrito/src/components/ItemCount.jsx
@@ -3,19 +3,22 @@ import { useState } from "react";
 
 const ItemCount = ({Stock, Initial, onAdd}) => {
     
+    //CONVIERTO LAS PROP A ESTADOS
     const [auxStock, setAuxStock] = useState(Stock);
     const [auxIntial, setAuxInitial] =useState(Initial);
 
+    //ESTABLEZCO EL VALO EN INICIAL EN EL PRIMER MONTAJE
     useEffect(() => {
         setAuxStock(auxIntial);
     },[])
     
-
+    //FUNCION PARA INCREMENTAR EL STOCK, CONDICIONADO POR EL MAXIMO STOCK CON OPERADORES TERNARIOS
     const aumentarStock = () => {
             
         auxStock < Stock ? setAuxStock(auxStock + 1) : setAuxStock(auxStock)
     }
 
+    //FUNCION PARA RESTAR EL STOCK, CONDICIONADO POR EL INITIAL STOCK CON OPERADORES TERNARIOS
     const disminuirStock = () => {
         auxStock >= Initial ? setAuxStock(auxStock - 1) : setAuxStock(auxStock)  
     }
