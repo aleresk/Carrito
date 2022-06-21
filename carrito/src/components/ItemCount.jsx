717c84b0a8edabded3e8a676e5ff4544df598ed6
aleresk <alejandro.resk@outlook.com>
2022-06-20 23:46:36 -0300

    carrio version 1

--- carrito/src/components/ItemCount.jsx
+++ carrito/src/components/ItemCount.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const ItemCount = ({Stock, Initial, onAdd}) => {
+    
+    const [auxStock, setauxStock] = useState(Stock);
+    const [auxIntial, setauxInitial] =useState(Initial);
+
+    
     return(
         <div>
             <p>El stock es: {Stock}</p>
