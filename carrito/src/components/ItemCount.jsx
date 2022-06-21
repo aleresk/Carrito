3d2cca73829cbea025f67ff83a11f0214b57ce5f
aleresk <alejandro.resk@outlook.com>
2022-06-21 01:59:04 -0300

    carrio version 1

--- carrito/src/components/ItemCount.jsx
+++ carrito/src/components/ItemCount.jsx
@@ -21,12 +21,12 @@ const ItemCount = ({Stock, Initial, onAdd}) => {
     }
 
     return(
-        <div>
+        <div className="contCarrito">
             <div className="cantProd">
-                <div ><button onClick={() => disminuirStock()}> - </button></div> <div>{auxStock}</div><div ><button onClick={() => aumentarStock()}> + </button></div>
+                <div ><button className="botonSuma" onClick={() => disminuirStock()}> - </button></div> <div>{auxStock}</div><div ><button className="botonSuma" onClick={() => aumentarStock()}> + </button></div>
             </div>
             <div>
-                <button onClick={() => onAdd()}>Agregar al Carrito</button>
+                <button className="botonAgregarCarrito" onClick={() => onAdd()}>Agregar al Carrito</button>
             </div>
         </div>
     )
