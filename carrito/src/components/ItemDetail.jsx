c66d4ec40507eb0da57b3e402dd9c074360283a4
aleresk <alejandro.resk@outlook.com>
2022-06-28 16:53:45 -0300

    carrio version 1

--- carrito/src/components/ItemDetail.jsx
+++ carrito/src/components/ItemDetail.jsx
@@ -1,5 +1,5 @@
 const ItemDetail = ({prod}) => {
     
-    return <div>El ID es: {prod.id} y el nombre es: {prod.nombre}</div> 
+    return <div>El ID es: {prod[0].id} y el nombre es: {prod[0].nombre}</div> 
 }
 export default ItemDetail;
