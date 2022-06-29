2b53398ff92ae8d696217f1789210c971e66fa8d
aleresk <alejandro.resk@outlook.com>
2022-06-29 01:42:03 -0300

    carrio version 1

--- carrito/src/components/ItemDetail.jsx
+++ carrito/src/components/ItemDetail.jsx
@@ -1,5 +1,24 @@
 const ItemDetail = ({prod}) => {
     
-    return <div>El ID es: {prod[0].id} y el nombre es: {prod[0].nombre}</div> 
+    return <div className="prodDetail">
+                <div>
+                    <img className="mainPic" src={prod[0].url} alt={prod[0].nombre}/>
+                </div>
+            <div>
+                <div className="classProd">
+                    <p className="titulo">Producto:</p>
+                    {prod[0].nombre} {prod[0].marca}
+                </div>
+                <div className="classProd">
+                    <p className="titulo">Precio:</p>
+                    ${prod[0].precio}
+                </div>
+                <div className="classProd">
+                    <p className="titulo">Stock</p>
+                    {prod[0].stock} Disponibles
+                </div>
+            </div>
+            </div>
+        
 }
 export default ItemDetail;
